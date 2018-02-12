17f44c71b21811cbe9c406b62d826368055a1d12
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-02-12 11:41:26 +0100

    Started the CRUD class

--- CDIO1/src/funktion/CRUD.java
+++ CDIO1/src/funktion/CRUD.java
@@ -1,5 +1,23 @@
 package funktion;
 
 public class CRUD {
+	
+	
+	
+	private void createUser(String username, String INI, String cpr, String role) {
+		
+	}
+	
+	private void deleteUser(String cpr ) {
+		
+	}
+	
+	private void updateUser(String cpr) {
+		
+	}
+	
+	private void readUser(String cpr) {
+		
+	}
 
 }
