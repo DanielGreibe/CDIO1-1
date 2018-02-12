c48a2bf111a3a32a5233edb5cd5bd180bc58d103
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-02-12 11:39:24 +0100

    Added innerClass called password

--- CDIO1/src/dto/UserDTO.java
+++ CDIO1/src/dto/UserDTO.java
@@ -4,34 +4,53 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDTO implements Serializable{
+public class UserDTO implements Serializable {
 
 	private static final long serialVersionUID = 4545864587995944260L;
-	private int	userId;                     
-	private String userName;                
-	private String ini;                 
+	private int userId;
+	private String userName;
+	private String ini;
 	private List<String> roles;
-	//TODO Add relevant fields
-	
+	// TODO Add relevant fields
+
+	class passWord {
+		
+		public passWord() {
+			
+		}
+		
+		private String generatePassWord() {
+			
+			return null;
+			
+		}
+
+	}
+
 	public UserDTO() {
 		this.roles = new ArrayList<>();
 	}
-	
+
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getIni() {
 		return ini;
 	}
+
 	public void setIni(String ini) {
 		this.ini = ini;
 	}
@@ -39,19 +58,21 @@ public class UserDTO implements Serializable{
 	public List<String> getRoles() {
 		return roles;
 	}
+
 	public void setRoles(List<String> roles) {
 		this.roles = roles;
 	}
-	
-	public void addRole(String role){
+
+	public void addRole(String role) {
 		this.roles.add(role);
 	}
+
 	/**
 	 * 
 	 * @param role
 	 * @return true if role existed, false if not
 	 */
-	public boolean removeRole(String role){
+	public boolean removeRole(String role) {
 		return this.roles.remove(role);
 	}
 
@@ -59,7 +80,5 @@ public class UserDTO implements Serializable{
 	public String toString() {
 		return "UserDTO [userId=" + userId + ", userName=" + userName + ", ini=" + ini + ", roles=" + roles + "]";
 	}
-	
-	
-	
+
 }
