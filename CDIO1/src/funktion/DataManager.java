314bd8559f62957f7852a43de27d07a1db099a1d
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-02-15 17:31:39 +0100

    The methods in dataManager should be finished, and have the relevant try/catch statements

--- CDIO1/src/funktion/DataManager.java
+++ CDIO1/src/funktion/DataManager.java
@@ -5,43 +5,69 @@ import java.util.List;
 import data.UserDTO;
 
 public class DataManager implements IUserDAO {
-	
+
 	private List<UserDTO> users;
 
 	@Override
 	public UserDTO getUser(int userId) throws DALException {
-	
-		return users.get(userId);
+		try {
+			int j = 0;
+			for (UserDTO i : users) {
+				if (i.getUserId() == userId) {
+					j = users.indexOf(userId);
+				}
+			}
+
+			return users.get(j);
+			
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Denne bruger findes ikke");
+		}
+		
+		return null;
 	}
 
 	@Override
 	public List<UserDTO> getUserList() throws DALException {
-		
+
 		return users;
 	}
 
 	@Override
 	public void createUser(UserDTO user) throws DALException {
-		
+
+		user = new UserDTO();
+
 		users.add(user);
-		
+
 	}
 
 	@Override
 	public void updateUser(UserDTO user) throws DALException {
-		
-		int i = user.getUserId();
-		
-		users.set(i-11, user);
-		
-		
+
+		int i = users.indexOf(user);
+
+		users.set(i, user);
+
 	}
 
 	@Override
 	public void deleteUser(int userId) throws DALException {
-		
-		users.remove(userId);
-		
+
+		try {
+			int j = 0;
+			for (UserDTO i : users) {
+				if (i.getUserId() == userId) {
+					j = users.indexOf(userId);
+				}
+			}
+
+			users.remove(j);
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Du kan ikke slette en bruger,"
+					+ "som allerede ikke eksisterer");
+		}
+
 	}
 
 }
