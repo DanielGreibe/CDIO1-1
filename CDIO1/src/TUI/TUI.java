82fecee5272911e440042e8addc8f1e117feac3e
Erick <User@10.16.99.128>
2018-03-04 13:29:48 +0100

    Refactored all methods from TUI into the ScaleApplication.
    Can easily be refactored into a third class if need be.

--- CDIO1/src/TUI/TUI.java
+++ CDIO1/src/TUI/TUI.java
@@ -1,32 +1,22 @@
 package TUI;
 
-import java.util.List;
 import java.util.Scanner;
 
-import data.UserDTO;
-import funktion.DataManager;
-import funktion.IUserDAO;
 import funktion.IUserDAO.DALException;
 
 public class TUI {
 
-	private DataManager data;
 	private Scanner keyb;
-	private boolean running;
 
 	public TUI() {
-		data = new DataManager();
 		keyb = new Scanner(System.in);
-		running = true;
+	}
+	
+	public void printText(String input) {
+		System.out.println(input);
 	}
 
-	public void mainMenu() throws DALException {
-		System.out.println("--------------------------------------------------------");
-		System.out.println("-");
-		System.out.println("-\t Welcome to CRUD International!");
-		System.out.println("-");
-		System.out.println("--------------------------------------------------------");
-		while (running) {
+	public String mainMenu() throws DALException {
 			System.out.println("");
 			System.out.println("");
 			System.out.println("What would you like to do?");
@@ -36,232 +26,33 @@ public class TUI {
 			System.out.println("\t3. Update User");
 			System.out.println("\t4. Delete User");
 			System.out.println("\t5. See All Users");
-			System.out.println("\t6. Exit Program");
+			System.out.println("\t6. Exit CRUD");
 			System.out.println("");
-			switch (getUserInput()) {
+			switch (nextInt()) {
 			case 1:
-				createUser();
-				break;
+				return "createUser";
 			case 2:
-				readUser();
-				break;
+				return "readUser";
 			case 3:
-				updateUser();
-				break;
+				return "updateUser";
 			case 4:
-				deleteUser();
-				break;
+				return "deleteUser";
 			case 5:
-				seeAllUsers();
-				break;
+				return "seeAllUsers";
 			case 6:
-				running = false;
-				break;
+				return "exit";
 			default:
-				System.out.println("Didn't understand input. Type an integer.");
+				return "fail";
 			}
 		}
-		System.out.println("Program now exiting. Goodbye!");
+
+
+	public int nextInt() {
+		return keyb.nextInt();
 	}
 
-	private void seeAllUsers() throws DALException {
-
-		System.out.println("Printing all users:");
-		System.out.println("");
-		List<UserDTO> userList = data.getUserList();
-		if (userList.isEmpty())
-			System.err.println("There are no users.");
-		for (UserDTO userDTO : userList) {
-			System.out.println(userDTO);
-		}
-
-	}
-
-	private void deleteUser() throws DALException {
-		int ID;
-		System.out.println("Type User-ID of what User you wish to delete.");
-		ID = keyb.nextInt();
-		data.deleteUser(ID);
-		System.out.println("User deleted.");
-
-	}
-
-	private void updateUser() throws DALException {
-		// All attributes required.
-		UserDTO tempUser = new UserDTO();
-		String username = null;
-		String initials = null;
-		String CPR = null;
-		int ID;
-		String role = null;
-		boolean moreRoles = true;
-
-		System.out.println("You are now updating a user.");
-		System.out.println("");
-
-		System.out.println("Type User-ID of user you wish to change. List of all users is printed here:");
-		seeAllUsers();
-		do {
-			ID = keyb.nextInt();
-		} while (!checkUserExistsID(ID));
-		tempUser.setUserId(ID);
-		System.out.println("You wish to edit user with ID " + tempUser.getUserId());
-		System.out.println("");
-
-		System.out.println("Type new username. Must be atleast 2 and maximally 20 characters");
-		do {
-			username = keyb.next();
-		} while (!checkUsername(username));
-		tempUser.setUserName(username);
-		System.out.println("New name set as " + tempUser.getUserName());
-		System.out.println("");
-
-		System.out.println("Type new initials. Must be atleast 2 and maximally 4 characters");
-		do {
-			initials = keyb.next();
-		} while (!checkInitials(initials));
-		tempUser.setIni(initials);
-		System.out.println("Initials set as " + tempUser.getIni());
-		System.out.println("");
-
-		System.out.println("Type new CPR-number.");
-		CPR = keyb.next();
-		tempUser.setCPR(CPR);
-		System.out.println("CPR set as " + tempUser.getCPR());
-		System.out.println("");
-
-		while (moreRoles) {
-			System.out.println("Type a role to add to the user. You must retype all established roles. Type DONE to finish.");
-			role = keyb.next();
-			if (role.equals("DONE"))
-				break;
-			tempUser.addRole(role);
-			System.out.println("");
-		}
-		System.out.println("");
-		System.out.println("User update complete. Saving to datamanager.");
-		data.updateUser(tempUser);
-		System.out.println("Saving to datamanager successful.");
-	}
-	
-	private boolean checkUserExistsID(int iD) throws DALException {
-		List<UserDTO> tempUserList = data.getUserList();
-			for (int i = 0; i < tempUserList.size(); i++) {
-				if (tempUserList.get(i).getUserId() == iD) {
-					System.out.println("User found.");
-					return true;
-				}
-			}
-		System.out.println("User-ID does not exist already. Try again.");
-		return false;
-	}
-
-	// TODO Make printout much nicer.
-	private void readUser() throws DALException {
-		UserDTO tempUser = null;
-		int ID;
-		System.out.println("Type User-ID of what User you wish to read.");
-		ID = keyb.nextInt();
-		tempUser = data.getUser(ID);
-		if (tempUser != null && tempUser.getUserName() != null)
-			System.out.println(tempUser.toString());
-		else
-			System.err.println("Could not find user.");
-
-	}
-
-	private void createUser() throws DALException {
-		// All attributes required.
-		UserDTO tempUser = new UserDTO();
-		String username = null;
-		String initials = null;
-		String CPR = null;
-		int ID;
-		String role = null;
-		boolean moreRoles = true;
-
-		System.out.println("You are now creating a user.");
-		System.out.println("");
-
-		System.out.println("Type a username. Must be atleast 2 and maximally 20 characters");
-		do {
-			username = keyb.next();
-		} while (!checkUsername(username));
-		tempUser.setUserName(username);
-		System.out.println("Name set as " + tempUser.getUserName());
-		System.out.println("");
-
-		System.out.println("Type initials. Must be atleast 2 and maximally 4 characters");
-		do {
-			initials = keyb.next();
-		} while (!checkInitials(initials));
-		tempUser.setIni(initials);
-		System.out.println("Initials set as " + tempUser.getIni());
-		System.out.println("");
-
-		System.out.println("Type CPR-number.");
-		CPR = keyb.next();
-		tempUser.setCPR(CPR);
-		System.out.println("CPR set as " + tempUser.getCPR());
-		System.out.println("");
-
-		System.out.println("Type a User-ID. Must be between and including 11 to 99");
-		do {
-			ID = keyb.nextInt();
-		} while (!checkUserID(ID));
-		tempUser.setUserId(ID);
-		System.out.println("User-ID set as " + tempUser.getUserId());
-		System.out.println("");
-
-		while (moreRoles) {
-			System.out.println("Type a role to add to the user. Type DONE to finish.");
-			role = keyb.next();
-			if (role.equals("DONE"))
-				break;
-			tempUser.addRole(role);
-			System.out.println("");
-		}
-		System.out.println("");
-		System.out.println("Users password is: " + tempUser.getPassword());
-		System.out.println("");
-		System.out.println("User creation complete. Saving to datamanager.");
-		data.createUser(tempUser);
-		System.out.println("Saving to datamanager successful.");
-	}
-
-	private boolean checkUserID(int iD) throws DALException {
-		List<UserDTO> tempUserList = data.getUserList();
-		if (iD >= 11 && iD <= 99) {
-			for (int i = 0; i < tempUserList.size(); i++) {
-				if (tempUserList.get(i).getUserId() == iD) {
-					System.out.println("User-ID is invalid. Try again.");
-					return false;
-				}
-			}
-			return true;
-		}
-		System.out.println("User-ID is invalid. Try again.");
-		return false;
-	}
-
-	private boolean checkInitials(String initials) {
-		if (initials.length() >= 2 && (initials.length()) <= 4)
-			return true;
-		System.out.println("Initials are invalid. Try again.");
-		return false;
-	}
-
-	private boolean checkUsername(String username) {
-		if (username.length() >= 2 && (username.length()) <= 20)
-			return true;
-		System.out.println("Username is invalid. Try again.");
-		return false;
-	}
-
-	private int getUserInput() {
-		int input;
-		input = keyb.nextInt();
-		return input;
+	public String nextString() {
+		return keyb.next();
 	}
 
 }
--- CDIO1/src/funktion/ScaleApplication.java
+++ CDIO1/src/funktion/ScaleApplication.java
@@ -1,78 +1,312 @@
 package funktion;
 
+import java.util.List;
+
+import TUI.TUI;
+import data.UserDTO;
 import funktion.IUserDAO.DALException;
 
-public class ScaleApplication 
-{
+public class ScaleApplication {
+
+	private boolean running = true;
 	DataManager datalag = new DataManager();
 	int tareWeight;
+	TUI textInterface = new TUI();
+	String print;
+
 	/**
-	 * Method to write text to the Scale with a String as input.
-	 * The String represents the text that is sent to the scale.
+	 * Method to write text to the Scale with a String as input. The String
+	 * represents the text that is sent to the scale.
+	 * 
+	 * @throws DALException
 	 */
-	public void writeText(String text)
-	{
-		//Nedenstående er commanden for at skrive tekst til vægten.
-		//Hvordan det virker? Dunno.
-		String scaleCommand = "D " + text + "crlf";
-		//Weight.Input(scaleCommand);
+
+	public ScaleApplication() throws DALException {
+		print = "--------------------------------------------------------\n-\n\t Welcome to Scale Application.\n-\n--------------------------------------------------------";
+		textInterface.printText(print);
+
+		// TODO: This shouldn't run before actually asked by a main menu method.
+		CRUDMenu();
+
 	}
-	
-	public boolean waitForConfirmation()
-	{
-		if (Weight.buttonPressed = "OK")
-		{
+
+	public void CRUDMenu() throws DALException {
+		String action;
+		while (running) {
+			action = textInterface.mainMenu();
+			switch (action) {
+			case "createUser":
+				createUser();
+				break;
+			case "readUser":
+				readUser();
+				break;
+			case "updateUser":
+				updateUser();
+				break;
+			case "deleteUser":
+				deleteUser();
+				break;
+			case "seeAllUsers":
+				seeAllUsers();
+				break;
+			case "exit":
+				running = false;
+				break;
+			default:
+				print = "Did not understand query. Please retry.";
+				textInterface.printText(print);
+				break;
+			}
+		}
+	}
+
+	public void seeAllUsers() throws DALException {
+
+		textInterface.printText("Printing all users.");
+		List<UserDTO> userList = datalag.getUserList();
+		if (userList.isEmpty())
+			textInterface.printText("There are no users.");
+		for (UserDTO userDTO : userList) {
+			textInterface.printText(userDTO.toString());
+		}
+
+	}
+
+	public void deleteUser() throws DALException {
+		int ID;
+		textInterface.printText("Type ID of user you wish to delete.");
+		ID = textInterface.nextInt();
+		datalag.deleteUser(ID);
+		textInterface.printText("User deleted.");
+
+	}
+
+	public void updateUser() throws DALException {
+		// All attributes required.
+		UserDTO tempUser = new UserDTO();
+		String username = null;
+		String initials = null;
+		String CPR = null;
+		int ID;
+		String role = null;
+		boolean moreRoles = true;
+
+		textInterface.printText("You are now updating a user.\n");
+
+		textInterface.printText("Type ID of user you wish to change. All users are printed here: \n");
+		seeAllUsers();
+		do {
+			ID = textInterface.nextInt();
+		} while (!checkUserExistsID(ID));
+		tempUser.setUserId(ID);
+		print = "You wish to edit user with ID " + tempUser.getUserId();
+		textInterface.printText(print);
+
+		print = "Type a username between 2 and 20 characters long.";
+		textInterface.printText(print);
+		do {
+			username = textInterface.nextString();
+		} while (!checkUsername(username));
+		tempUser.setUserName(username);
+		print = "New name set as " + tempUser.getUserName();
+		textInterface.printText(print);
+
+		print = "Type initials between 2 and 4 characters long.";
+		textInterface.printText(print);
+		do {
+			initials = textInterface.nextString();
+		} while (!checkInitials(initials));
+		tempUser.setIni(initials);
+		print = "New initials set as " + tempUser.getIni();
+		textInterface.printText(print);
+
+		textInterface.printText("Type CPR-Number.");
+		CPR = textInterface.nextString();
+		tempUser.setCPR(CPR);
+		print = "New CPR set as " + tempUser.getCPR();
+		textInterface.printText(print);
+
+		while (moreRoles) {
+			textInterface.printText("Type a role to add to the user. Type DONE to finish.\n");
+			role = textInterface.nextString();
+			if (role.equals("DONE"))
+				break;
+			tempUser.addRole(role);
+			textInterface.printText("Role added.");
+		}
+		System.out.println("");
+		print = "User creation complete. Saving to datamanager...";
+		textInterface.printText(print);
+		datalag.updateUser(tempUser);
+		textInterface.printText("Saving to datamanager successful.");
+	}
+
+	public boolean checkUserExistsID(int iD) throws DALException {
+		List<UserDTO> tempUserList = datalag.getUserList();
+		for (int i = 0; i < tempUserList.size(); i++) {
+			if (tempUserList.get(i).getUserId() == iD) {
+				textInterface.printText("User found.");
+				return true;
+			}
+		}
+		textInterface.printText("User not found.");
+		return false;
+	}
+
+	public void readUser() throws DALException {
+		UserDTO tempUser = null;
+		int ID;
+		textInterface.printText("Type User-ID of user you wish to see.");
+		ID = textInterface.nextInt();
+		tempUser = datalag.getUser(ID);
+		if (tempUser != null && tempUser.getUserName() != null)
+			textInterface.printText(tempUser.toString());
+		else
+			textInterface.printText("Could not find user.");
+
+	}
+
+	private void createUser() throws DALException {
+		// All attributes required.
+		UserDTO tempUser = new UserDTO();
+		String username = null;
+		String initials = null;
+		String CPR = null;
+		int ID;
+		String role = null;
+		boolean moreRoles = true;
+
+		print = "You are now creating a user.\n";
+		textInterface.printText(print);
+
+		print = "Type a username between 2 and 20 characters long.";
+		textInterface.printText(print);
+		do {
+			username = textInterface.nextString();
+		} while (!checkUsername(username));
+		tempUser.setUserName(username);
+		print = "Name set as " + tempUser.getUserName() + "\n";
+		textInterface.printText(print);
+
+		print = "Type initials between 2 and 4 characters long.";
+		textInterface.printText(print);
+		do {
+			initials = textInterface.nextString();
+		} while (!checkInitials(initials));
+		tempUser.setIni(initials);
+		print = "Initials set as " + tempUser.getIni() + "\n";
+		textInterface.printText(print);
+
+		textInterface.printText("Type CPR-Number.");
+		CPR = textInterface.nextString();
+		tempUser.setCPR(CPR);
+		print = "CPR set as " + tempUser.getCPR() + "\n";
+		textInterface.printText(print);
+
+		textInterface.printText("Type User-ID. Must be between 11 and 99.");
+		do {
+			ID = textInterface.nextInt();
+		} while (!checkUserID(ID));
+		tempUser.setUserId(ID);
+		print = "User-ID set as " + tempUser.getCPR() + "\n";
+		textInterface.printText(print);
+
+		while (moreRoles) {
+			textInterface.printText("Type a role to add to the user. Type DONE to finish.\n");
+			role = textInterface.nextString();
+			if (role.equals("DONE"))
+				break;
+			tempUser.addRole(role);
+			textInterface.printText("Role added.");
+		}
+		print = "Users password is: " + tempUser.getPassword() + "\nUser creation complete. Saving to datamanager...";
+		textInterface.printText(print);
+		datalag.createUser(tempUser);
+		textInterface.printText("Saving to datamanager successful.");
+	}
+
+	public boolean checkUserID(int iD) throws DALException {
+		List<UserDTO> tempUserList = datalag.getUserList();
+		if (iD >= 11 && iD <= 99) {
+			for (int i = 0; i < tempUserList.size(); i++) {
+				if (tempUserList.get(i).getUserId() == iD) {
+					textInterface.printText("User-ID invalid. Try again.");
+					return false;
+				}
+			}
 			return true;
 		}
-		else
-		{
+		textInterface.printText("User-ID invalid. Try again.");
+		return false;
+	}
+
+	public boolean checkInitials(String initials) {
+		if (initials.length() >= 2 && (initials.length()) <= 4)
+			return true;
+		textInterface.printText("Initials invalid. Try again.");
+		return false;
+	}
+
+	public boolean checkUsername(String username) {
+		if (username.length() >= 2 && (username.length()) <= 20)
+			return true;
+		textInterface.printText("Username invalid. Try again.");
+		return false;
+	}
+
+	public void writeText(String text) {
+		// Nedenstï¿½ende er commanden for at skrive tekst til vï¿½gten.
+		// Hvordan det virker? Dunno.
+		String scaleCommand = "D " + text + "crlf";
+		// Weight.Input(scaleCommand);
+	}
+
+	/* Hvad er Weight? */
+	public boolean waitForConfirmation() {
+		if (Weight.buttonPressed = "OK") {
+			return true;
+		} else {
 			return false;
 		}
 	}
-	
-	public String getScaleInput()
-	{
-//		return Scale.getValue()
+
+	public String getScaleInput() {
+		// return Scale.getValue()
 		return "InsertScaleInputHere";
 	}
-	
-	public void AskForID() throws DALException
-	{
-		//Først skrives til vægten, venter på et okay, tager værdien fra vægten
-		//og henter brugeren med samme ID og udskriver navnen på den bruger
-		//og venter til sidst på et ok fra brugeren.
+
+	public void AskForID() throws DALException {
+		// Fï¿½rst skrives til vï¿½gten, venter pï¿½ et okay, tager vï¿½rdien fra vï¿½gten
+		// og henter brugeren med samme ID og udskriver navnen pï¿½ den bruger
+		// og venter til sidst pï¿½ et ok fra brugeren.
 		writeText("Indtast ID");
 		waitForConfirmation();
-		String StringID = getScaleInput();	
+		String StringID = getScaleInput();
 		int ID = Integer.parseInt(StringID);
 		String username = datalag.getUser(ID).getUserName();
 		writeText(username);
-		waitForConfirmation();	
+		waitForConfirmation();
 	}
-	
-	public void askForBatch() throws DALException
-	{
+
+	public void askForBatch() throws DALException {
 		writeText("Indtast ID");
 		waitForConfirmation();
-		String StringBatch = getScaleInput();	
+		String StringBatch = getScaleInput();
 		int Batch = Integer.parseInt(StringBatch);
-//		Hent Batch værdien.
-//		Skriv tekst med Batchen ud til brugeren med writeText
+		// Hent Batch vï¿½rdien.
+		// Skriv tekst med Batchen ud til brugeren med writeText
 		waitForConfirmation();
 	}
-	
-	public void resetScale()
-	{
+
+	public void resetScale() {
 		writeText("Clear Scale");
 		waitForConfirmation();
 	}
-	
-	public void saveTare()
-	{
-//		tareWeight = Send input to Scale ("S clrf");
-		
+
+	public void saveTare() {
+		// tareWeight = Send input to Scale ("S clrf");
+
 	}
-	
-	
-	
+
 }
