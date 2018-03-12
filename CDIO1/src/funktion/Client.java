15ddd684261a3388ac36230ba898c9dc30baf3b6
Daniel Greibe <s175207@student.dtu.dk>
2018-03-12 15:57:02 +0100

    Outsourced functionality in Client class.

--- CDIO1/src/funktion/Client.java
+++ CDIO1/src/funktion/Client.java
@@ -2,62 +2,71 @@ package funktion;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
- 
+
 public class Client {
- 
-   public static void main(String argv[])
-      {
-	   try{
-		   //Makes a Scanner that is used to read the input to the Scale.
-		   	Scanner keyboard = new Scanner(System.in);
-		   	
-		   	//Makes a Socket with 169.254.2.2 as the IP and 8000 as Port.
-		    Socket socketClient= new Socket("169.254.2.2",8000);
-		    System.out.println("Client: "+"Connection Established");
- 
-		    //Makes a BufferedReader and Writer to write and read from the Socket.
-		    BufferedReader reader = 
-		    		new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
- 
-		    BufferedWriter writer= 
-	        		new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
-		    
-		    
-		    String scaleMessage;
-		    System.out.println("Write your command below:");
-		    
-		    //Input  holds the value of the command you want to send to the scale.
-		    String input = "";
-		    
-		    //stopMessage is the value you have to give input through the scanner to stop the continuous loop,
-		    //that read, and writes to an from the scale.
-		    String stopMessage = "Stop";
-		    
-		    //Continuous loop to write to the scale and receive an answer.
-		    //Prints the message received from the scale.
-		    
-		    // Close all resources.
-		    keyboard.close();
-		    socketClient.close();
-		    
-		    while (!input.equals(stopMessage)) 
-		    {
-		    	System.out.println("Write a input on the keyboard:");
-		    	input = keyboard.nextLine();
-			    writer.write(input +"\r\n");
-			    writer.flush();
-				if((scaleMessage = reader.readLine()) != null)
-				{
-					//Prints out the answer received from the scale.
-					System.out.println("Client: " + scaleMessage);
-				}
-			}
-		    
- 
-	   }catch(Exception e){e.printStackTrace();}
-      }
+
+	Scanner keyboard = new Scanner(System.in);
+	Socket socketClient;
+	BufferedReader reader;
+	BufferedWriter writer;
+
+	public void Connect(String IP , int Portnumber) throws IOException
+	{
+		try
+		{
+			//Makes a Scanner that is used to read the input to the Scale.
+
+
+			//Makes a Socket with 169.254.2.2 as the IP and 8000 as Port.
+			socketClient = new Socket("169.254.2.2",8000);
+			System.out.println("Client: "+"Connection Established");
+
+			//Makes a BufferedReader and Writer to write and read from the Socket.
+			reader = 
+					new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+
+			writer = 
+					new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));  
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			// Close all resources.
+			keyboard.close();
+			socketClient.close();
+		}
+	}
+
+	public void SendCommand(String Command) throws IOException
+	{
+		System.out.println("Write a input on the keyboard:");
+		Command = keyboard.nextLine();
+		writer.write(Command +"\r\n");
+		writer.flush();
+	}
+
+
+	public String ReadOutput() throws IOException
+	{
+		String output;
+		if((output = reader.readLine()) != null)
+		{
+			//Prints out the answer received from the scale.
+			System.out.println("Client: " + output);
+			return output;
+		}
+		else
+		{
+			return output;
+		}
+	}
 }
